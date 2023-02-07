a5a8baeaedbad826225a5a441b4c4ebaa66e11c5
Dmitriy Gorskin <dagorskin@yandex.ru>
2023-02-07 17:50:52 +0300

    Basic done!

--- DOMSummary/main.js
+++ DOMSummary/main.js
@@ -3,7 +3,14 @@ h1.textContent = 'Конспект DOM' // Добавление текста в 
 document.body.prepend(h1) // Добавление DOM элемента 'h1' в начало тега HTML 'body' (или DOM элемента 'body').
 h1.id = 'h1-id' // Изменение 'id' элемента (тега) 'h1'.
 h1.classList.add('h1-class') // Добавление класса элементу (тегу) 'h1'.
+h1.classList.remove('h1-class') // Удаление класа у элемента (тега) 'h1'
+h1.classList.toggle('h1-class') // 'Переключение' класса. Если он есть - будет убран, если его нет - будет добавлен и вернет true/false.
 h1.className = 'h1-class-changed' // Изменение класса элемента (тега) 'h1'. Если нет класса, то создаст.
+h1.classList.contains('h1-class-changed') // Проверка на наличие класса у элемента (тега) 'h1'. Вернет true/false.
+document.querySelector('.h1-class-changed') // Найти ПЕРВЫЙ элемент (тэг) в документе. В данном случае по классу '.class', если по id то '#id'.
+document.querySelectorAll('.h1-class-changed') // Найти и выбрать ВСЕ элемент в документе. Вернет массив элементов.
+document.getElementsByClassName('h1-class-changed') // Найти элементы (теги) с классом 'h1-class-changed'. Возвращает коллекцию.
+document.getElementsByTagName('h1') // Найти все элементы (теги) с названием 'h1'. Возвращает коллекцию.
 
 const list = [ // Создание массива с DOM элементами (строки списка 'li').
   document.createElement('li'),
@@ -17,7 +24,7 @@ list[1].textContent = 'Вторая строка списка.'
 list[2].textContent = 'Третья строка списка.'
 list[3].textContent = 'Четвертая строка списка, которая будет удалена'
 list[4].innerHTML = '<strike>Зачеркнутая строка.</strike>' // Добавление текста в тег 'li' через нулевой индекс массива. Текст НЕ игнорирует внутри себя HTML теги.
-const ol = document.getElementById('ol') // Поиск элемента HTML по id и присваивание в переменную.
+const ol = document.getElementById('ol') // Поиск элемента HTML по id и присваивание в переменную. Быстрее чем метод 'querySelector'.
 ol.append(list[0], list[1]) // Добавление двух первых строк списка в список 'ol'.
 list[1].after(list[2]) // Добавление третьей строки списка ПОСЛЕ второй в список 'ol'.
 list[1].before(list[3]) // Добавление четвертой строки списка ДО третьей в список 'ol'.
